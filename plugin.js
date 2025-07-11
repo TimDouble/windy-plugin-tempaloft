c3cc9b0c6bd358377b6786c60bc0167a2bccaf94
TimDouble <tjd5185@gmail.com>
2025-07-11 13:09:48 -0500

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,26 +1,6 @@
 export default {
-  name: 'tempaloft',
-
-  setup(params) {
-    const { store } = params.windyAPI;
-
-    const levels = ['FL050', 'FL100', 'FL180', 'FL240', 'FL300', 'FL340'];
-    let currentLevelIndex = 0;
-
-    function updateOverlay() {
-      const level = levels[currentLevelIndex];
-      store.set('overlay', 'temp');
-      store.set('level', level);
-    }
-
-    window.tempaloft = {
-      nextLevel: () => {
-        currentLevelIndex = (currentLevelIndex + 1) % levels.length;
-        updateOverlay();
-      },
-      currentLevel: () => levels[currentLevelIndex]
-    };
-
-    updateOverlay();
+  name: "windy-plugin-tempaloft",
+  setup() {
+    console.log("Plugin loaded");
   }
 };
